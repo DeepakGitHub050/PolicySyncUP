833f393e5c896c2f839eb1d633270673a3d43206
DeepakGitHub050 <ds1008076@gmail.com>
2024-07-31 12:30:33 +0530

    initial commit

---
+++ src/main/java/com/dksapp/policysyncup/PolicySyncUpApplication.java
@@ -0,0 +1,13 @@
+package com.dksapp.policysyncup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicySyncUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolicySyncUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dksapp/policysyncup/PolicySyncUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dksapp.policysyncup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicySyncUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
